20e76969f05f7a5b2873f6668162af606c594460
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-07 17:07:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import sqlite3
 import pandas as pd
 import hashlib
 import uuid
+import time
+
 
 # --- Helper Functions ---
 def hash_password(password):
@@ -119,6 +121,7 @@ if not st.session_state.authenticated:
                     st.session_state.authenticated = True
                     st.session_state.username = reg_username
                     st.session_state.role = reg_role
+                    time.sleep(2)  # Add a short delay
                     st.experimental_rerun()
                 except sqlite3.IntegrityError:
                     st.error("Username already exists. Please choose a different one.")
